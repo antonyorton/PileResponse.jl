9cb346d881535c25d7175ef447128c50cf34d48d
antonyorton <antony.orton@hotmail.com>
2025-02-08 11:11:18 +1100

    first commit

--- src/PileResponse.jl
+++ src/PileResponse.jl
@@ -3,7 +3,7 @@ module PileResponse
 export helloworld
 
 function helloworld()
-    print("Hi there PileResponse.js world")
+    print("Hi there PileResponse.js world, let's go")
     return 123
 end
 
